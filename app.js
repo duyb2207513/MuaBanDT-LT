6020f9a0fa9451ded8cf33da12a9acf98bee8f4f
duyb2207513 <duyb2207513@student.ctu.edu.vn>
2025-01-14 19:15:08 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 const contactsRouter=require("./app/routes/contact.route");
+const ApiError=require("./app/api-error");
+
 
 app.use(cors());
 app.use(express.json());
-
 app.use("/api/contacts",contactsRouter);
 
+//handle 404 response
+app.use((req,res,next)=>{
+    //code o day se chay khi khong co route nao dc dinh nghia
+    return next(new ApiError(404,"Resource not found"));
+});
+app.use((err,req,res,next)=>{
+    //Middleware xy ly loi taprung
+    return res.status(error.statusCode||500).json({
+        message: error.message ||"Internal Server Error",
+    });
+});
 app.get("/",(req,res)=>{
     res.json({message: "Welcomt to my app."});
 });
